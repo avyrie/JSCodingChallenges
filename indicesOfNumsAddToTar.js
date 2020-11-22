29f989d13bd89f4700aeccd0dfabf1ffe895a7b4
avyrie <pittman.ally@gmail.com>
2020-11-21 18:39:49 -0700

    added return indices in an array where the values add up to a target num, return numbers in an array that add up to a target number

---
+++ indicesOfNumsAddToTar.js
@@ -0,0 +1,18 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
+
+// Solution #1
+
+const twoIndices = function(nums, target) {
+    let arr = [];
+    for (let j = 0; j <= nums.length; j++) {
+        for (let i = 0; i <= nums.length; i++) {
+            if(i !== j && nums[i] + nums[j] === target) {
+                arr.push(j, i)
+                return arr
+            } 
+        }
+    }
+}
+let thisNums = [3, 9, 8, 3];
+let thisTarget = 6;
+console.log(twoIndices(thisNums, thisTarget))
---
+++ twoNumSumTarget.js
@@ -0,0 +1,18 @@
+// Given an array of integers (nums) and an integer (target) return 2 numbers in the array that add up to the target
+
+// Solution #1
+
+const twoSum = function(nums, target) {
+    let arr = [];
+    for (let j = 0; j < nums.length; j ++) {
+        for (let i = 0; i < nums.length; i++) {
+            if(j !== i && nums[i] + nums[j] === target) {
+                arr.push(nums[i], nums[j])
+                return arr
+            } 
+        }
+    }
+}
+let thisNums = [3, 9, 2, 5, 0, 1, 11];
+let thisTarget = 11;
+console.log(twoSum(thisNums, thisTarget))
