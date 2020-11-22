7f3507250565674a758e110065e2a36fb9040e6f
avyrie <pittman.ally@gmail.com>
2020-11-22 16:17:30 -0700

    added to atLEastFive.js

--- atLeastFive.js
+++ atLeastFive.js
@@ -1,6 +1,6 @@
 // Create a function that prints all numbers starting at 1 up to and including a given argument. No matter tha argument, the function should print each number starting at 1 up to and including 5
 
-// Solution #1 
+// ----- Solution #1 -----
 
 function atLeastFive(num) {
     if(num < 5) {
@@ -11,3 +11,13 @@ function atLeastFive(num) {
     } 
 }
 atLeastFive(4)
+
+
+// ----- Solution 2 -----
+
+function logAtLEastFive(num) {
+    for (let i = 1; i <= Math.max(5, num); i++) {
+        console.log(i)
+    }
+}
+logAtLEastFive(10)
